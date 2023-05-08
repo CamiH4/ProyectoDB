6b3e619c80e817d2228e87a7ca1e028f8ec37279
Camilo Solis <camilo.solis@est.uca.edu.ni>
2023-05-08 12:31:39 -0600

    Agregué interfáz gráfica con conexión a la BD

---
+++ RegistroPersonas/Form1.Designer.vb
@@ -0,0 +1,103 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ArchivosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FormulariosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CiudadToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PersonaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ArchivosToolStripMenuItem, Me.FormulariosToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ArchivosToolStripMenuItem
+        '
+        Me.ArchivosToolStripMenuItem.Name = "ArchivosToolStripMenuItem"
+        Me.ArchivosToolStripMenuItem.Size = New System.Drawing.Size(65, 20)
+        Me.ArchivosToolStripMenuItem.Text = "Archivos"
+        '
+        'FormulariosToolStripMenuItem
+        '
+        Me.FormulariosToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CiudadToolStripMenuItem, Me.PersonaToolStripMenuItem})
+        Me.FormulariosToolStripMenuItem.Name = "FormulariosToolStripMenuItem"
+        Me.FormulariosToolStripMenuItem.Size = New System.Drawing.Size(77, 20)
+        Me.FormulariosToolStripMenuItem.Text = "Formulario"
+        '
+        'CiudadToolStripMenuItem
+        '
+        Me.CiudadToolStripMenuItem.Name = "CiudadToolStripMenuItem"
+        Me.CiudadToolStripMenuItem.Size = New System.Drawing.Size(116, 22)
+        Me.CiudadToolStripMenuItem.Text = "Ciudad"
+        '
+        'PersonaToolStripMenuItem
+        '
+        Me.PersonaToolStripMenuItem.Name = "PersonaToolStripMenuItem"
+        Me.PersonaToolStripMenuItem.Size = New System.Drawing.Size(116, 22)
+        Me.PersonaToolStripMenuItem.Text = "Persona"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.RegistroPersonas.My.Resources.Resources.cliente
+        Me.PictureBox1.Location = New System.Drawing.Point(142, 46)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(535, 378)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Registro de Personas"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ArchivosToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FormulariosToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CiudadToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PersonaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ RegistroPersonas/forms/FrmCiudad.Designer.vb
@@ -0,0 +1,201 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmCiudad
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Activo = New System.Windows.Forms.CheckBox()
+        Me.TxbNombre = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TxbId = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.BtnNuevo = New System.Windows.Forms.ToolStripButton()
+        Me.BtnGuardar = New System.Windows.Forms.ToolStripButton()
+        Me.BtnEditar = New System.Windows.Forms.ToolStripButton()
+        Me.BtnEliminar = New System.Windows.Forms.ToolStripButton()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.DgvCiudades = New System.Windows.Forms.DataGridView()
+        Me.GroupBox1.SuspendLayout()
+        Me.ToolStrip1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.DgvCiudades, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Activo)
+        Me.GroupBox1.Controls.Add(Me.TxbNombre)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.TxbId)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 28)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(572, 197)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'Activo
+        '
+        Me.Activo.AutoSize = True
+        Me.Activo.Location = New System.Drawing.Point(253, 149)
+        Me.Activo.Name = "Activo"
+        Me.Activo.Size = New System.Drawing.Size(56, 17)
+        Me.Activo.TabIndex = 4
+        Me.Activo.Text = "Activo"
+        Me.Activo.UseVisualStyleBackColor = True
+        '
+        'TxbNombre
+        '
+        Me.TxbNombre.Location = New System.Drawing.Point(139, 96)
+        Me.TxbNombre.Name = "TxbNombre"
+        Me.TxbNombre.Size = New System.Drawing.Size(303, 20)
+        Me.TxbNombre.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(82, 104)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(47, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Nombre:"
+        '
+        'TxbId
+        '
+        Me.TxbId.Location = New System.Drawing.Point(139, 31)
+        Me.TxbId.Name = "TxbId"
+        Me.TxbId.Size = New System.Drawing.Size(303, 20)
+        Me.TxbId.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(82, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(21, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "ID:"
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BtnNuevo, Me.BtnGuardar, Me.BtnEditar, Me.BtnEliminar})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(602, 25)
+        Me.ToolStrip1.TabIndex = 1
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'BtnNuevo
+        '
+        Me.BtnNuevo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BtnNuevo.Image = Global.RegistroPersonas.My.Resources.Resources.add_file
+        Me.BtnNuevo.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.BtnNuevo.Name = "BtnNuevo"
+        Me.BtnNuevo.Size = New System.Drawing.Size(23, 22)
+        Me.BtnNuevo.Text = "Nuevo"
+        '
+        'BtnGuardar
+        '
+        Me.BtnGuardar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BtnGuardar.Image = Global.RegistroPersonas.My.Resources.Resources.verify
+        Me.BtnGuardar.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.BtnGuardar.Name = "BtnGuardar"
+        Me.BtnGuardar.Size = New System.Drawing.Size(23, 22)
+        Me.BtnGuardar.Text = "Guardar"
+        '
+        'BtnEditar
+        '
+        Me.BtnEditar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BtnEditar.Image = Global.RegistroPersonas.My.Resources.Resources.edit
+        Me.BtnEditar.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.BtnEditar.Name = "BtnEditar"
+        Me.BtnEditar.Size = New System.Drawing.Size(23, 22)
+        Me.BtnEditar.Text = "Editar"
+        '
+        'BtnEliminar
+        '
+        Me.BtnEliminar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.BtnEliminar.Image = Global.RegistroPersonas.My.Resources.Resources.close
+        Me.BtnEliminar.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.BtnEliminar.Name = "BtnEliminar"
+        Me.BtnEliminar.Size = New System.Drawing.Size(23, 22)
+        Me.BtnEliminar.Text = "Eliminar"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.DgvCiudades)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 247)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(572, 191)
+        Me.GroupBox2.TabIndex = 2
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Registros Guardados 0"
+        '
+        'DgvCiudades
+        '
+        Me.DgvCiudades.AllowUserToAddRows = False
+        Me.DgvCiudades.AllowUserToDeleteRows = False
+        Me.DgvCiudades.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DgvCiudades.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DgvCiudades.Location = New System.Drawing.Point(3, 16)
+        Me.DgvCiudades.Name = "DgvCiudades"
+        Me.DgvCiudades.ReadOnly = True
+        Me.DgvCiudades.Size = New System.Drawing.Size(566, 172)
+        Me.DgvCiudades.TabIndex = 0
+        '
+        'FrmCiudad
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(602, 450)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "FrmCiudad"
+        Me.Text = "Registro de Ciudades"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        CType(Me.DgvCiudades, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TxbNombre As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TxbId As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Activo As CheckBox
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents BtnNuevo As ToolStripButton
+    Friend WithEvents BtnGuardar As ToolStripButton
+    Friend WithEvents BtnEditar As ToolStripButton
+    Friend WithEvents BtnEliminar As ToolStripButton
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents DgvCiudades As DataGridView
+End Class
